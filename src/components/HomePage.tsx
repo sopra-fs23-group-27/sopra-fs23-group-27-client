25570961e769149324a20dae293387dbf592b65d
Dominic Tobler <dominic.tobler.ch@gmail.com>
2023-03-29 17:15:27 +0200

    Add websocket setup in HomePage component

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { useState } from "react";
+import { io } from "socket.io-client";
 
 const Container = styled.div`
   display: flex;
@@ -10,10 +12,37 @@ const Container = styled.div`
   font-size: 38px;
 `;
 export const HomePage = () => {
+  const socket = io("ws://localhost:8080");
+
+  socket.on("message", (message) => {
+    const extendedMessages = [...messages];
+    extendedMessages.push(message);
+    setMessages(extendedMessages);
+  });
+  const sendMessage = () => {
+    console.log("sending: ", currentMessage);
+    socket.emit("message", currentMessage);
+  };
+
+  const [messages, setMessages] = useState<Array<string>>(["Sample"]);
+  const [currentMessage, setCurrentMessage] = useState<string>("");
+
   return (
     <Container>
       <h1>Group 27</h1>
       <h1>FlagMania</h1>
+
+      <ul>
+        {messages.map((m, ind) => (
+          <li key={ind}>{m}</li>
+        ))}
+      </ul>
+      <input
+        placeholder="message"
+        value={currentMessage}
+        onChange={(e) => setCurrentMessage(e.currentTarget.value)}
+      />
+      <button onClick={() => sendMessage()}>Send</button>
     </Container>
   );
 };
