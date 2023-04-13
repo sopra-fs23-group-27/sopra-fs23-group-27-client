e23925e4ebba25af6bda1317f16ba94e046b3850
Dominic Tobler <63369752+DMC-CMD@users.noreply.github.com>
2023-04-13 12:18:40 +0100

    fix merge bug (#38)

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import "../styles/HomePage.css";
 import { Link } from "react-router-dom";
 import "../styles/HomePage.css";
-import { Link } from "react-router-dom";
 
 const Container = styled.div`
   display: flex;
