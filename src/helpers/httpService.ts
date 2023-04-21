2cb8af38a1ce8c712685748bf175ff1450c5f6bf
Dominic Tobler <dominic.tobler.ch@gmail.com>
2023-04-21 10:20:22 +0200

    add login view, closes #2

--- src/helpers/httpService.ts
+++ src/helpers/httpService.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 
-//const localURL = "https://localhost/8080";
+const localURL = "https://localhost/8080";
 const onlineURL = "https://sopra-fs23-group-27-server.oa.r.appspot.com";
 const mainURL = onlineURL;
 
 export const httpGet = (endpoint: string) => {
   return axios.get(mainURL + endpoint);
+};
+
+export const httpPost = (endpoint: string, body: Object) => {
+  return axios.post(mainURL + endpoint, body);
 };
