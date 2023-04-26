357a7e7a6ebe7bc8519ed615f024e28f77d25f8f
Noah Mamié <noah.mamie@uzh.ch>
2023-04-26 17:57:55 +0200

    lobby user table and clipboard button

--- src/views/GameLobby.tsx
+++ src/views/GameLobby.tsx
@@ -1,19 +1,46 @@
 import { Link, useParams } from "react-router-dom";
 import { useSubscription, useStompClient } from "react-stomp-hooks";
 import { useEffectOnce } from "../customHooks/useEffectOnce";
+import { useState } from "react";
+import styled from "styled-components";
+import { UsersRolesTable } from "../components/UserTable";
+
+const UserContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
+  font-size: 38px;
+`;
+
+const GreenButton = styled.button`
+  width: 200px;
+  height: 50px;
+  background-color: #90ee90;
+  border: 1px solid #000;
+  border-radius: 5px;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+`;
 
 export const GameLobby = () => {
   const { lobbyId } = useParams();
   const stompClient = useStompClient();
 
   // get the lobby name from local storage
-  const lobbyName = localStorage.getItem("lobbyName");
-
-  // get the private key from local storage
-  const privateLobbyKey = localStorage.getItem("privateLobbyKey");
+  const [lobbyName, setLobbyname] = useState("");
+  const [joinedPlayerNames, setJoinedPlayerNames] = useState<string[]>([]);
 
   // get the player token from local storage
   const playerToken = localStorage.getItem("token");
+
+  // map playername to name and role
+  const playerNames = joinedPlayerNames.map((playerName: string) => {
+    return { name: playerName, role: "player" };
+  });
+  
 
   console.log("player token: ", playerToken);
 
@@ -31,8 +58,12 @@ export const GameLobby = () => {
   useSubscription(
     `/user/queue/lobby/${lobbyId}/lobby-settings`,
     (message: any) => {
-      const lobbyId = JSON.parse(message.body).lobbyId as string;
-      console.log("Message from server: ", lobbyId);
+      const lobbyName = JSON.parse(message.body).lobbyName as string;
+      const joinedPlayerNames = JSON.parse(message.body).joinedPlayerNames as string[];
+      console.log("Message from server: ", lobbyName);
+      console.log("Message from server: ", joinedPlayerNames);
+      setLobbyname(lobbyName);
+      setJoinedPlayerNames(joinedPlayerNames);
     }
   );
 
@@ -49,15 +80,15 @@ export const GameLobby = () => {
     >
       <h1>Game Lobby {lobbyId}: {lobbyName}</h1>
       <h2>Waiting for players to join...</h2>
-      
+
       <h3>Players in lobby:</h3>
-      <ul>
-        <li>Player 1</li>
-        <li>Player 2</li>
-        <li>Player 3</li>
-      </ul>
-      <Link to={`/lobbies/${lobbyId}/game`}>
-        <button>Start Game</button>
+
+      <UserContainer>
+        <UsersRolesTable data={playerNames} />
+      </UserContainer>
+
+      <Link to={`/gameRound`}>
+        <GreenButton>Start Game</GreenButton>
       </Link>
     </div>
   );
