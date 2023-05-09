182652bc45929b4b1f1a57d1b2a64c1eab074bda
nmamie <91463286+nmamie@users.noreply.github.com>
2023-05-09 08:36:18 +0200

    enable dynamic URL (#74)

---
+++ src/helpers/config.ts
@@ -0,0 +1,9 @@
+const dev = {
+    apiUrl: 'http://localhost:8080',
+  };
+  
+  const prod = {
+    apiUrl: 'https://sopra-fs23-group-27-server.oa.r.appspot.com',
+  };
+  
+  export const config = process.env.NODE_ENV === 'development' ? dev : prod;
